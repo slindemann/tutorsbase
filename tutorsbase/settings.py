da81b0475155475e0cfd13e9d878b4a2b952ac34
Sebastian Lindemann <sebastian.lindemann@physik.uni-freiburg.de>
2018-10-11 23:08:49 +0200

    Add admin.index to sidebar for superusers

--- tutorsbase/settings.py
+++ tutorsbase/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$+-pdchscf-_t2(*uo&fn7fvv3ivs_%pugtnj+wz63#72l!#zj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','0.0.0.0','10.4.73.214']
+ALLOWED_HOSTS = ['127.0.0.1','0.0.0.0','10.4.73.214','192.168.178.45']
 
 # SESSION SETTING
 
